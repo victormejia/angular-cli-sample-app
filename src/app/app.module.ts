eec35eb7173cef3d6674a39ec975f9a6f74be310
Victor Mejia <victor.a.mejia@gmail.com>
2020-10-17 14:18:29 -0700

    05-04: generate pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
+import { PhonePipe } from './pipes/phone.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactListComponent
+    ContactListComponent,
+    PhonePipe
   ],
   imports: [
     BrowserModule,
