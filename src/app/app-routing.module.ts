a20e07888884333d738f54700db258e5ba004da0
Victor Mejia <victor.a.mejia@gmail.com>
2020-10-17 14:40:07 -0700

    05-05: generate route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { ContactListComponent } from './contact-list/contact-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contacts',
+    component: ContactListComponent,
+    canActivate: [AuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
